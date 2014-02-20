92c6169dec5879859dce75da3faf5f28304eb41f
Juan Castillo <castillobgr@gmail.com>
2014-02-19 23:11:56 -0500

    Optimized query.

--- Webservice/test.js
+++ Webservice/test.js
@@ -19,24 +19,34 @@ server.listen(port, ipAdd, function(){
     console.log(server.name + ' escuchando por ' + server.url);
 });
 
+//function findChampionships(req, res, next){
+//    var pollas = [];
+//    console.log("entered " + req.params.userId);
+//    db.collection('chickens').find({}).toArray(function(err, chickens){
+//       if(err) throw err;
+//       
+//       chickens.forEach(function(chicken){
+//           var users = chicken.users;
+//           for(var i = 0; i < users.length; i++){
+//               if(users[i]['id'] == req.params.userId){
+//                   pollas.push(chicken);
+//               }
+//           }
+//       });
+//       res.send(200, pollas);
+//    });
+//    
+//    return next();
+//}
+
 function findChampionships(req, res, next){
-    var pollas = [];
     console.log("entered " + req.params.userId);
-    db.collection('chickens').find({}).toArray(function(err, chickens){
-       if(err) throw err;
-       
-       chickens.forEach(function(chicken){
-           var users = chicken.users;
-           for(var i = 0; i < users.length; i++){
-               if(users[i]['id'] == req.params.userId){
-                   pollas.push(chicken);
-               }
-           }
-       });
-       res.send(200, pollas);
+    db.collection('chickens').find({'users' : {'$elemMatch' : {'id' : parseInt(req.params.userId)}}}).toArray(function(err, chickens){
+       if(err) {throw err};
+       console.dir(chickens.length);
+       res.send(200, chickens);
+       return next();
     });
-    
-    return next();
 }
 
 
